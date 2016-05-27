73bb016d4647f47cc27a168b20f4c7a2fe712cf9
Leny BERNARD <leny@appventus.com>
2016-05-27 16:26:24 +0200

    move default controller / views into Front directory

---
+++ src/AppBundle/Controller/Front/DefaultController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Controller\Front;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+}
