8f0d40b516d75715af489bb88d7a6fb2443213e0
Leny BERNARD <leny@appventus.com>
2016-05-27 16:25:32 +0200

    composer require victoire/victoire, declare in Kernel and set config

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,10 +15,54 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new AppVentus\AlertifyBundle\AvAlertifyBundle(),
+            new AppVentus\AsseticInjectorBundle\AvAsseticInjectorBundle(),
+            new Bazinga\Bundle\JsTranslationBundle\BazingaJsTranslationBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new Infinite\FormBundle\InfiniteFormBundle(),
+            new JMS\AopBundle\JMSAopBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Snc\RedisBundle\SncRedisBundle(),
+            new Knp\DoctrineBehaviors\Bundle\DoctrineBehaviorsBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new A2lix\TranslationFormBundle\A2lixTranslationFormBundle(),
+
+            //Victoire bundles
+            new Victoire\Bundle\AnalyticsBundle\VictoireAnalyticsBundle(),
+            new Victoire\Bundle\BlogBundle\VictoireBlogBundle(),
+            new Victoire\Bundle\BusinessEntityBundle\VictoireBusinessEntityBundle(),
+            new Victoire\Bundle\BusinessPageBundle\VictoireBusinessPageBundle(),
+            new Victoire\Bundle\CoreBundle\VictoireCoreBundle(),
+            new Victoire\Bundle\CriteriaBundle\VictoireCriteriaBundle(),
+            new Victoire\Bundle\FilterBundle\VictoireFilterBundle(),
+            new Victoire\Bundle\FormBundle\VictoireFormBundle(),
+            new Victoire\Bundle\I18nBundle\VictoireI18nBundle(),
+            new Victoire\Bundle\MediaBundle\VictoireMediaBundle(),
+            new Victoire\Bundle\PageBundle\VictoirePageBundle(),
+            new Victoire\Bundle\QueryBundle\VictoireQueryBundle(),
+            new Victoire\Bundle\SeoBundle\VictoireSeoBundle(),
+            new Victoire\Bundle\SitemapBundle\VictoireSitemapBundle(),
+            new Victoire\Bundle\TemplateBundle\VictoireTemplateBundle(),
+            new Victoire\Bundle\TwigBundle\VictoireTwigBundle(),
+            new Victoire\Bundle\UserBundle\VictoireUserBundle(),
+            new Victoire\Bundle\ViewReferenceBundle\ViewReferenceBundle(),
+            new Victoire\Bundle\WidgetBundle\VictoireWidgetBundle(),
+            new Victoire\Bundle\WidgetMapBundle\VictoireWidgetMapBundle(),
+            new Victoire\Widget\ButtonBundle\VictoireWidgetButtonBundle(),
+            new Victoire\Widget\TextBundle\VictoireWidgetTextBundle(),
+
             new AppBundle\AppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
